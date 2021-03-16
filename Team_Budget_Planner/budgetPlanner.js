10381e51a21fa80973aba780a84c816bc9553672
Muzahir7 <muzahirh@smu.edu>
2021-03-15 23:53:30 -0500

    Fixed the code to show the annual budget below the table in the finance team page

--- Team_Budget_Planner/budgetPlanner.js
+++ Team_Budget_Planner/budgetPlanner.js
@@ -1,5 +1,4 @@
 var projects = [];
-var projects3 = [];
 
 function saveProjects(){
     //create an object that stores the data of the form.
@@ -14,12 +13,13 @@ function saveProjects(){
     //console.log(projects);
     localStorage.setItem('BudgetPlanner', JSON.stringify(projects));
 }
-
+var projects2 = [];
+var sum = 0;
 //create a function that retrieves the data from the sessionStorage and 
 //puts that data into a table.
 function genTable(){
     var retrievedData = localStorage.getItem('BudgetPlanner');
-    var projects2 = JSON.parse(retrievedData);
+    projects2 = JSON.parse(retrievedData);
 
  var html = '<table class= "tableClass">';
  html += '<tr>';
@@ -33,19 +33,17 @@ function genTable(){
   html += '<tr>';
   for( var j in projects2[i] ) {
     html += '<td class="tdClass">' + projects2[i][j] + '</td>';
+    
   }
+
+  sum = sum + parseInt(projects2[i].budget);
+
   html += '</tr>';
  }
+
+ console.log(sum);
+
  html += '</table>';
  document.getElementById('container').innerHTML = html;
 
- projects3 = projects2;
- 
 }
-var sum = 0;
-//console.log(parseInt(projects3[0][3]));
-/*function totalSum(){
-    for (var i = 0; i < projects3.length; i++){
-        sum = sum + parseInt(projects3[i].getElementById("budget"));
-    }
-}*/
